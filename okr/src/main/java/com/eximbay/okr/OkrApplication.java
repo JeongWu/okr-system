c230070bfb0c50bd5d36c287ec8a47495c1778de
JeongWu <park961003@naver.com>
2021-03-26 09:03:24 +0900

    Build project

---
+++ okr/src/main/java/com/eximbay/okr/OkrApplication.java
@@ -0,0 +1,13 @@
+package com.eximbay.okr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OkrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OkrApplication.class, args);
+	}
+
+}
